4afd807bbbd42d22c6b6f979ec8a571a009c0ed1
L.Walker <logantwalker95@gmail.com>
2020-09-16 15:34:39 -0400

    model and migration updated

--- migrations/20200916191222-create-user.js
+++ migrations/20200916191222-create-user.js
@@ -1,4 +1,11 @@
 'use strict';
+//to create the tables in your database, you will need to run the following commands in the command line with sequelize cli
+//--------------------//
+//sequelize db:migrate
+
+//side note: make sure you are using the name of a database on your local machine in the config.json "development" section
+//you also need to add the password to YOUR local mysql connection in config.json
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
@@ -8,8 +15,22 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userId: {
-        type: Sequelize.INTEGER
+      email: {
+        allowNull:false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      firstName: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        allowNull:true,
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
