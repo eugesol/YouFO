b50096427af5934a39e2f4e9434d03c87f4c5591
L.Walker <logantwalker95@gmail.com>
2020-09-19 11:38:04 -0400

    building index route

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+// Requiring path to so we can use relative routes to our HTML files
+const path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("index");
+  });
+}
+
